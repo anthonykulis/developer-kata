d2d1f733a86c5a3a54526dd23f3992f2829345ed
Anthony Kulis <anthony@saasindustries.com>
2018-07-12 16:50:27 -0500

    fixes typo

--- src/redux/stores/index.js
+++ src/redux/stores/index.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import promise from 'redux-promise-middleware';
 import catfishReducer from '../reducers/';
 
 export default createStore(
-    catfishReducer,
+    combineReducers({ catfish: catfishReducer }),
     applyMiddleware(
         promise(),
         createLogger()

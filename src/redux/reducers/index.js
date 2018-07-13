db0c04c4b3bda2aebd6d9580183835f9b43d2481
hhoburg <hhoburg@gmail.com>
2018-07-13 10:04:35 -0700

    adds fish species and fish sorted by length to redux state.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,21 +1,33 @@
 import constants from '../constants';
 
 const initState = {
-    all: []
+  all: [],
+  sortedByLength: [],
+  species: [],
 };
 
 export default (state = initState, action) => {
   switch (action.type) {
     case constants.FETCH_ALL_FISH_FULFILLED:
-      const catfish = action.payload.data.map(fish => {
+      const all = action.payload.data.map(fish => {
         return {
           ...fish,
           weight: Math.round(fish.length * fish.girth * fish.girth / 800)
         }
       });
+      const species = [];
+      all.forEach(fish => {
+        if (species.indexOf(fish.species) < 0) {
+          species.push(fish.species);
+        }
+      });
+      const sortedByLength = all.sort((a, b) => {
+        return b.length - a.length;
+      });
       return {
         ...state,
-        all: catfish,
+        all,
+        sortedByLength
       };
     default:
       return state;

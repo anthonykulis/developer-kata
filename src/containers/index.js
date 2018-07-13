19fd59fe2f743c6b16f7378ff7845d3e5dd9968a
hhoburg <hhoburg@gmail.com>
2018-07-13 09:47:18 -0700

    adds list of all catfish, debounced input for listing an angler's top 5 fish, and minimal styling

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,8 +1,17 @@
 import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
+import {DebounceInput} from 'react-debounce-input';
 import {fetchAllFish} from '../redux/actions';
 
 class Index extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      anglerSearch: '',
+      currentAngler: []
+    }
+  }
 
   componentWillMount() {
     this.props.fetchAllFish();
@@ -10,8 +19,79 @@ class Index extends Component {
 
   render() {
     return (
-      <h1>Something</h1>
+      <div className='row'>
+        <div className='col-md-4'>
+          {/*LISTS ALL FISH*/}
+          {this.props.catfish.all.slice(0, 10).map(fish => {
+            return (
+              <div key={fish.id}>
+                <p>
+                  Angler: {fish.angler}
+                </p>
+                <p>
+                  Species: {fish.species}
+                </p>
+                <p>
+                  Length: {fish.length}
+                </p>
+                <p>
+                  Girth: {fish.girth}
+                </p>
+              </div>
+            )
+          })}
+        </div>
+        <div className='col-md-4'>
+          {/*LISTS TOP FIVE FISH BY ANGLER*/}
+          <DebounceInput
+            minLength={2}
+            debounceTimeout={500}
+            value={this.state.anglerSearch}
+            onChange={event => {
+              const currentAngler = this.props.catfish.all.filter(fish =>
+                fish.angler.toLowerCase() === event.target.value.toLowerCase()
+              ).sort((a, b) => {
+                return b.length - a.length;
+              });
+              this.setState({
+                anglerSearch: event.target.value,
+                currentAngler
+              })
+            }}
+          />
+            {this.state.currentAngler.length < 1 ? (
+              <p>Type a valid angler above</p>
+            ) : (
+              <Fragment>
+                <h3>{this.state.anglerSearch}'s Top 5 fish</h3>
+                {this.state.currentAngler.slice(0, 5).map(fish => {
+                  return (
+                    <div key={fish.id} className='fish-box'>
+                      <p>
+                        Species: {fish.species}
+                      </p>
+                      <p>
+                        Length: {fish.length}
+                      </p>
+                      <p>
+                        Girth: {fish.girth}
+                      </p>
+                    </div>
+                  );
+                })}
+              </Fragment>
+            )}
+        </div>
+        <div className='col-md-4'>
+        </div>
+      </div>
     )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    catfish: state.catfish
   }
 }
 
@@ -23,4 +103,4 @@ function mapDispatchToProps(dispatch) {
   })
 }
 
-export default connect(null, mapDispatchToProps)(Index);
+export default connect(mapStateToProps, mapDispatchToProps)(Index);
